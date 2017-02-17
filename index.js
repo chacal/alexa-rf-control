281f72248b91fbc9ea0497f5810f0c4786f3c4eb
Jouni Hartikainen <jouni.hartikainen@reaktor.fi>
2017-02-17 21:41:44 +0200

    var -> const

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-var awsIot = require('aws-iot-device-sdk')
-var lambda = require('./skill-adapter-lambda.js')
-var express = require('express')
-var rcswitch = require('rcswitch-gpiomem')
-var mqtt = require('mqtt')
+const awsIot = require('aws-iot-device-sdk')
+const lambda = require('./skill-adapter-lambda.js')
+const express = require('express')
+const rcswitch = require('rcswitch-gpiomem')
+const mqtt = require('mqtt')
 
-var HTTP_PORT = 3000
-var MQTT_BROKER = 'mqtt://ha-opi'
+const HTTP_PORT = 3000
+const MQTT_BROKER = 'mqtt://ha-opi'
 rcswitch.enableTransmit(17)
 
 startAwsIoTListener()
 startHttpListener()
-var mqttClient = startMqttClient(MQTT_BROKER)
+const mqttClient = startMqttClient(MQTT_BROKER)
 
 
 function startAwsIoTListener() {
-  var device = awsIot.device({
+  const device = awsIot.device({
     keyPath: './iot-us-east-1-private.pem.key',
     certPath: './iot-us-east-1-certificate.pem.crt',
     caPath: './ca.pem',
@@ -31,20 +31,20 @@ function startAwsIoTListener() {
 
   device
     .on('message', function(topic, payload) {
-      var payloadJson = JSON.parse(payload.toString())
+      const payloadJson = JSON.parse(payload.toString())
       console.log('Message from AWS IoT:', topic, payloadJson)
       handleEvent(payloadJson)
     })
 }
 
 function startHttpListener() {
-  var app = express()
-  var bodyParser = require('body-parser')
+  const app = express()
+  const bodyParser = require('body-parser')
 
   app.get('/switch/:applianceId', (req, res) => res.end())
 
   app.post('/switch/:applianceId', bodyParser.text({type: '*/*'}), function (req, res) {
-    var event = { applianceId: req.params.applianceId, event: req.body.toLowerCase() === 'on' ? 'TurnOnRequest' : 'TurnOffRequest' }
+    const event = { applianceId: req.params.applianceId, event: req.body.toLowerCase() === 'on' ? 'TurnOnRequest' : 'TurnOffRequest' }
     handleEvent(event)
     res.end()
   })
@@ -54,9 +54,7 @@ function startHttpListener() {
 
 function startMqttClient(brokerUrl) {
   const client = mqtt.connect(brokerUrl)
-  client.on('connect', function () {
-    console.log("Connected to MQTT server")
-  })
+  client.on('connect', () => console.log("Connected to MQTT server"))
   return client
 }
 
@@ -76,7 +74,7 @@ function handleEvent(event) {
   function switchOn(appliance) { switchOnOrOff(appliance, true)}
   function switchOff(appliance) { switchOnOrOff(appliance, false)}
   function switchOnOrOff(appliance, switchOn) {
-    var rfConfig = appliance.rfConfig
+    const rfConfig = appliance.rfConfig
     if(appliance && rfConfig) {
       if(switchOn)
         rcswitch.switchOn(rfConfig.family, rfConfig.group, rfConfig.device)
